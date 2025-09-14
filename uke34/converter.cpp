868cb6cef74921cfdc361bb464c0b8b5d68eb183
jdredcliffe <jdredcliffe@gmail.com>
2025-09-15 01:34:32 +0200

    Update converter.cpp

--- uke34/converter.cpp
+++ uke34/converter.cpp
@@ -1,1 +1,14 @@
-James Dean Redcliffe
+#include <iostream>
+
+double inchesToCentimeters(double inches) {
+    return inches * 2.54;
+}
+
+int main() {
+    double inches;
+    std::cout << "Enter value in inches: ";
+    std::cin >> inches;
+    double centimeters = inchesToCentimeters(inches);
+    std::cout << inches << " inches is " << centimeters << " centimeters." << std::endl;
+    return 0;
+}
